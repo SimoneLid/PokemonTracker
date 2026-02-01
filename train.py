469d5a887c60fa953e3878d8e6380048a839cdae
DoctorWho28 <69338485+DoctorWho28@users.noreply.github.com>
2026-02-01 13:51:27 +0100

    modifica train.py

--- train.py
+++ train.py
@@ -2,10 +2,14 @@ from multiprocessing import freeze_support
 from ultralytics import YOLO
 from sys import argv
 import torch
+import shutil
+import os
 
 if __name__ == "__main__":
     if torch.cuda.is_available():
         freeze_support()
+
+        model_name = argv[3]
 
         model = YOLO(argv[1]) 
 
@@ -18,6 +22,22 @@ if __name__ == "__main__":
             batch=16,
             workers=8,
         )
+        print("\n--- Inizio salvataggio del modello migliore ---")
+        source_path = model.trainer.best
+
+        dest_folder = "models"
+        os.makedirs(dest_folder, exist_ok=True)
+
+        if not model_name.endswith('.pt'):
+            model_name += '.pt'
+            
+        dest_path = os.path.join(dest_folder, model_name)
+
+        try:
+            shutil.copy(source_path, dest_path)
+            print(f"Successo: Modello copiato da '{source_path}' a '{dest_path}'")
+        except Exception as e:
+            print(f"Errore durante la copia del modello: {e}")
 
     else:
         print("Cuda non presente sulla macchina")
