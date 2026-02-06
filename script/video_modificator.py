73136108c5f82a06d7416ff45b9b93a778c82f58
DoctorWho28 <69338485+DoctorWho28@users.noreply.github.com>
2026-02-06 16:33:57 +0100

    Il bro odia le doppie virgolette

--- script/video_modificator.py
+++ script/video_modificator.py
@@ -63,7 +63,7 @@ class VideoAnnotator:
     def get_label_filename(self, frame_idx=None):
         """Genera il percorso del file label. Se frame_idx è None, usa il corrente."""
         idx = frame_idx if frame_idx is not None else self.current_frame_idx
-        return os.path.join(self.output_dir, f"{self.video_name.split("_")[2]}_{idx:06d}.txt")
+        return os.path.join(self.output_dir, f"{self.video_name.split('_')[2]}_{idx:06d}.txt")
 
     def update_consecutive_count(self):
         """Calcola quanti frame consecutivi (incluso questo all'indietro) sono salvati."""
