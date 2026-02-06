e85f10f32efd008c4472f266ac2e3ce8e8112570
DoctorWho28 <69338485+DoctorWho28@users.noreply.github.com>
2026-02-06 16:28:19 +0100

    frame video 13

--- Test_predict/mostra_predict.py
+++ Test_predict/mostra_predict.py
@@ -6,8 +6,9 @@ import numpy as np
 
 # --- 1. CONFIGURAZIONE PATH E IMPORT ---
 FILE = Path(__file__).resolve()
-ROOT = FILE.parents[0]
+ROOT = FILE.parents[0].parents[0]
 YOLO_PATH = ROOT / "yolov5"
+print(f"YOLO_PATH: {YOLO_PATH}")
 
 if str(YOLO_PATH) not in sys.path:
     sys.path.append(str(YOLO_PATH))
@@ -19,15 +20,15 @@ from utils.augmentations import letterbox
 # --- 2. CARICAMENTO MODELLO ---
 print("Caricamento modello...")
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-weights_path = YOLO_PATH / "runs" / "train" / "exp10" / "weights" / "best.pt" 
+weights_path = YOLO_PATH / "runs" / "train" / "exp29" / "weights" / "best.pt" 
 
 model = torch.hub.load(str(YOLO_PATH), 'custom', path=str(weights_path), source='local')
 model.to(device).eval()
 
 # --- 3. CARICAMENTO IMMAGINI ---
-path_img_t_minus_1 = "frame_000067_Zona_2_02.jpg"
-path_img_t = "frame_000068_Zona_2_02.jpg"
-path_img_output = "frame_000069_Zona_2_02.jpg"
+path_img_t_minus_1 = "Test_predict/frame_000067_Zona_2_02.jpg"
+path_img_t = "Test_predict/frame_000068_Zona_2_02.jpg"
+path_img_output = "Test_predict/frame_000069_Zona_2_02.jpg"
 
 img1 = cv2.imread(path_img_t_minus_1)
 img2 = cv2.imread(path_img_t)
@@ -42,8 +43,7 @@ img_size = 640
 img1_res = letterbox(img1, img_size, stride=32, auto=False)[0]
 img2_res = letterbox(img2, img_size, stride=32, auto=False)[0]
 
-img_stacked = np.concatenate((img1_res, img2_res), axis=2)
-img_tensor = img_stacked.transpose((2, 0, 1))[::-1] 
+img_tensor = img1_res.transpose((2, 0, 1))[::-1] 
 img_tensor = np.ascontiguousarray(img_tensor)
 img_tensor = torch.from_numpy(img_tensor).to(device).float() / 255.0
 img_tensor = img_tensor[None]
@@ -55,6 +55,16 @@ with torch.no_grad():
 pred = non_max_suppression(pred, conf_thres=0.25, iou_thres=0.45)
 det = pred[0]
 
+img_tensor = img2_res.transpose((2, 0, 1))[::-1] 
+img_tensor = np.ascontiguousarray(img_tensor)
+img_tensor = torch.from_numpy(img_tensor).to(device).float() / 255.0
+img_tensor = img_tensor[None]
+
+with torch.no_grad():
+    pred = model(img_tensor)
+
+pred = non_max_suppression(pred, conf_thres=0.25, iou_thres=0.45)
+det = pred[0]
 # --- 6. DISEGNO SU TUTTE E TRE LE IMMAGINI ---
 # Creiamo tre copie separate per l'output
 out1, out2, out3 = img1.copy(), img2.copy(), img3.copy()
